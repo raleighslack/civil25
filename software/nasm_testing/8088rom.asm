7fe8cb7eace7fd750c070eb70e4876e4ea0472e4
cyrilscott <raleighslack@gmail.com>
2022-09-11 13:07:46 -0400

    update NASM

--- software/nasm_testing/8088rom.asm
+++ software/nasm_testing/8088rom.asm
@@ -1,20 +1,20 @@
         CPU 8086
 		
 ROMSIZE equ 262144
-ROMCS   equ 0C000h
+ROMCS   equ 0xC0000
 INIT_SS equ 07000h
 INIT_SP equ 0FFF0h
 
         org 10000h-ROMSIZE
 
 start:
-		mov ax, 0xffff
-		mov bx, 0x1234
+		mov ah, 0x12
+		mov al, 0x34
 		jmp start
 		
 		
 times ROMSIZE-16-($-$$) db 0xFF
 
-        jmp     0xC0000
+        jmp     0xC000:0x0000
 
 times ROMSIZE-($-$$) db 0xFF
