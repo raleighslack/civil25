a17ed9bd12b6d26d41b2ebd1c15f931f505db797
cyrilscott <raleighslack@gmail.com>
2022-09-24 20:26:03 +0000

    Efficient Hello World

--- software/nasm_testing/8088rom.asm
+++ software/nasm_testing/8088rom.asm
@@ -9,77 +9,43 @@ INIT_SP equ 0FFF0h
         org 10000h-ROMSIZE
 
 start:
+		mov ax, 0x7000 ;loads where stack will be; has to be 64k
+		mov ss, ax
+		xor sp, sp ;sets stack pointer to 0
+
+		mov ax, 0xc000 ;sets data segment to C000 - Will change
+		mov ds, ax
+
 		mov al, 0x80 ;move 10000000 into AL
-		out 03h, al ;Move AL into 82c55 control register, mode 0, all ports are output
-		mov al, 0x00 ;Sets control register to 0; just to be safe
+		out 03h, al ;Move AL into 82c55 control register; setting I/O chip to mode 0, all ports are output
+		mov al, 0x00 ;Sets control register of lcd to 0; just to be safe
 		out 01h, al
 
 		mov al, 00111000b ;Set 8 bit mode, 2-line display, 5x8 font
-		out 00h, al ;send to LCD
 		call writeControl
 
 		mov al, 00001111b ;Display on, cursor on, blink on
-		out 00h, al
 		call writeControl
 
 		mov al, 00000110b ;Increment and shift cursor; don't shift display
-		out 00h, al
 		call writeControl ;Write to control register of LCD
 
-		mov al, "H" ;Write H to LCD
-		out 00h, al
-		call writeLetter ;Jumps to writeLetter, then returns
+		mov al, 00000010b
+		call writeControl
 
-		mov al, "e" ;Write e to LCD
-		out 00h, al
-		call writeLetter
-
-		mov al, "l" ;Write l to LCD
-		out 00h, al
-		call writeLetter
-
-		mov al, "l" ;Write l to LCD
-		out 00h, al
-		call writeLetter
-		
-		mov al, "o" ;Write o to LCD
-		out 00h, al
-		call writeLetter
-
-		mov al, "," ;Write , to LCD
-		out 00h, al
-		call writeLetter
-		
-		mov al, " " ;Write ( ) to LCD
-		out 00h, al
-		call writeLetter
-
-		mov al, "W" ;Write W to LCD
-		out 00h, al	
-		call writeLetter
-
-		mov al, "o" ;Write o to LCD
-		out 00h, al
-		call writeLetter
-
-		mov al, "r" ;Write r to LCD
-		out 00h, al
-		call writeLetter
-
-		mov al, "l" ;write l to LCD
-		out 00h, al
-		call writeLetter
-
-		mov al, "d" ;Write d to LCD
-		out 00h, al
-		call writeLetter
-
-		mov al, "!" ;Write ! to LCD
-		out 00h, al
-		call writeLetter
+writeLCDString:
+		mov cx, message_end - message ;loads CX with # of characters
+		mov si, message ;loads pointer into appropriate register
+writeLoop:
+		lodsb ;wierd instruction; loads AL with data from DS:SI with offset
+		call writeLetter ;writes letter from AL
+		dec cx ;decreses # of characters to write
+		jnz writeLoop ;if its not done, jump back up
+		jmp loop ;otherwise jump to loop
 
 writeControl: ;writes to control register of LCD
-		mov al, 0x00 ;Turn off register select pin
+		out 00h, al
+		xor al, al ;Turn off register select pin
 		out 01h, al
 		mov al, 00000100b ;Turn on enable pin, sending the instruction 
 		out 01h, al
@@ -88,6 +54,7 @@ writeControl: ;writes to control register of LCD
 		ret
 
 writeLetter: ;writes to data register of LCD
+		out 00h, al
 		mov al, 00000001b ;Turn on register select pin
 		out 01h, al
 		mov al, 00000101b ;Turn on register select pin + enable pin
@@ -96,13 +63,12 @@ writeLetter: ;writes to data register of LCD
 		out 01h, al
 		ret
 
-loop: ;Flashes LED connected to PORTC
-		mov al, 11111111b ;set AL all high
-		out 02h, al ;set all of PORT C pins high
-		mov al, 00000000b ;set AL all low
-		out 02h, al ;set all of PORT C pins low
+loop:
 		jmp loop
-		
+
+message: 
+	db "Hello, world!"
+message_end:
 		
 times ROMSIZE-16-($-$$) db 0xFF
 
